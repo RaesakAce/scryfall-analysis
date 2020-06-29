9ff8f280f3c8a589cfb735bd8bda8a76c421378b
RaesakAce <marco.ace.casaglia@gmail.com>
2020-06-29 13:21:00 +0200

    Update data-cleaning.py

--- data-cleaning.py
+++ data-cleaning.py
@@ -1,1 +1,8 @@
-import pandas
+import pandas as pd
+import seaborn as sb
+
+data_path='.\data\oracle-cards-20200629050643.json'
+data=pd.read_json(data_path)
+
+useful_columns=['id','name','mana_cost','cmc', 'type_line', 'oracle_text', 'colors', 'color_identity', 'keywords', 'legalities', 'games', 'reserved', 'foil', 'nonfoil','promo', 'reprint','set', 'border_color', 'frame', 'full_art', 'textless','power', 'toughness']
+print (data.head())
