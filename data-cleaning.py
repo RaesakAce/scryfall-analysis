bb49371ce50724e5b92d55b5f4e3cfc9bb26f955
RaesakAce <marco.ace.casaglia@gmail.com>
2020-06-29 17:29:03 +0200

    Update data-cleaning.py

--- data-cleaning.py
+++ data-cleaning.py
@@ -6,6 +6,6 @@ data_path='.\data\oracle-cards-20200629050643.json'
 data=pd.read_json(data_path)
 
 useful_columns=['id','name','mana_cost','cmc', 'type_line', 'oracle_text', 'colors', 'color_identity', 'keywords', 'legalities', 'games', 'reserved', 'foil', 'nonfoil','promo', 'reprint','set', 'border_color', 'frame', 'full_art', 'textless','power', 'toughness']
-print(data.head())
+print(data[useful_columns])
 print(useful_columns)
 gui.alert('everything was printed')
