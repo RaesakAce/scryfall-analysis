7e94fa6c86f99f5f2ff04496c15682e566730fa5
RaesakAce <marco.ace.casaglia@gmail.com>
2020-07-03 19:59:32 +0200

    Update data-cleaning.py

--- data-cleaning.py
+++ data-cleaning.py
@@ -5,11 +5,32 @@ import pyautogui as gui
 data_path='.\data\oracle-cards-20200629050643.json'
 data=pd.read_json(data_path)
 
-prices=data['prices']
-prices=prices['eur']
-print(prices)
-useful_columns=['id','name','mana_cost','cmc', 'type_line', 'oracle_text', 'colors', 'color_identity', 'keywords', 'legalities', 'games', 'reserved', 'foil', 'nonfoil','promo', 'reprint','set', 'border_color', 'frame', 'full_art', 'textless','power', 'toughness']
+
+useful_columns=['id'
+,'name'
+,'mana_cost'
+,'cmc'
+, 'type_line'
+, 'oracle_text'
+, 'colors'
+, 'color_identity'
+, 'keywords'
+, 'legalities'
+, 'games'
+, 'reserved'
+, 'foil'
+, 'nonfoil'
+,'promo'
+, 'reprint'
+,'set'
+, 'border_color'
+, 'frame'
+, 'full_art'
+, 'textless'
+,'power'
+, 'toughness']
 print(data['edhrec_rank'])
+print(type(data['prices']))
 print(data.columns)
-#sb.relplot(x="edhrec_rank", y="prices",data=data);
+sb.barplot(x="edhrec_rank",data=data);
 gui.alert('everything was printed')
