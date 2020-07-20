c1534b6d279dce798a55a80e4893a18a590997c3
RaesakAce <marco.ace.casaglia@gmail.com>
2020-07-20 12:09:54 +0200

    made an updater
    If the data from scryfall isn't updated, there is no reason to download it again

--- data_download.py
+++ data_download.py
@@ -1,5 +1,7 @@
 import urllib3
 import pandas as pd
+import os
+import time
 
 class dataDownload():
     def api_request():
@@ -15,9 +17,12 @@ class dataDownload():
         if (r.status == 200):
             pd.read_json(r.data).to_json(data_path)
     
-    def main(data_path):
-        data_download.save_json(data_download.api_request(),data_path)
+    def update(data_path):
+        last_update=(time.time()-os.stat(data_path).st_mtime)/3600
+        if (last_update > 24):
+            dataDownload.save_json(dataDownload.api_request(),data_path)
+        else: print(f'The data has been updated {round(last_update,2)} hours ago')
 
 if (__name__ == '__main__') :
     data_path='.\data\oracle-cards.json'
-    data_download.main(data_path)
+    dataDownload.update(data_path)
